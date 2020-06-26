6b8cc83a005e852e5fa890ff44defcf78288c1fc
manntera <contact@manntera.com>
2020-06-27 01:41:03 +0900

    [Add]ProcedualModelBaseを追加しました。

---
+++ Assets/SDK/ProceduralModelingKit/Scripts/ProceduralModelBase.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+
+namespace OGL.ProceduralModelingKit
+{
+    [ExecuteInEditMode]
+    [RequireComponent(typeof(MeshFilter), typeof(MeshRenderer))]
+
+    public abstract class ProceduralModelBase : MonoBehaviour
+    {
+        private MeshFilter MeshFilterComponent
+        {
+            get
+            {
+                if (meshFilterComponent == null)
+                {
+                    meshFilterComponent = GetComponent<MeshFilter>();
+                }
+
+                return meshFilterComponent;
+            }
+        }
+
+        private MeshRenderer MeshRendererComponent
+        {
+            get
+            {
+                if (meshRendererComponent == null)
+                {
+                    meshRendererComponent = GetComponent<MeshRenderer>();
+                }
+
+                return meshRendererComponent;
+            }
+        }
+
+        [SerializeField] private MeshFilter meshFilterComponent;
+        [SerializeField] private MeshRenderer meshRendererComponent;
+
+        protected abstract Mesh CreateMesh();
+    }
+}
